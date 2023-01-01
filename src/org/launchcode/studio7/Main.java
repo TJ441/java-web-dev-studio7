74d7898e17ba03b059af80de4bfe5f9d2b8ef5af
Tim Johnson <timothy.johnson@zohomail.com>
2023-01-01 09:22:32 -0600

    Used LC solution to see what they wanted

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -3,10 +3,19 @@ package org.launchcode.studio7;
 public class Main {
 
   public static void main(String[] args){
+    CD cd = new CD("CD example", 700, "CD-R", 350);
+    DVD dvd = new DVD("DVD example", 4700, "DVD-R", 1450);
 
-    // TODO: Declare and initialize a CD and a DVD object.
+    cd.spinDisc();
+    dvd.spinDisc();
 
-    // TODO: Call each CD and DVD method to verify that they work as expected.
+    cd.readData();
+    dvd.readData();
 
+    System.out.println(cd.writeData(275));
+    System.out.println(dvd.writeData(8000));
+
+    System.out.println(cd.diskInfo());
+    System.out.println(dvd.diskInfo());
   }
 }
